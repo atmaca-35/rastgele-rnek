8ca6a74fd6c7937fdaed42763d67e69a3ec82ff6
atmaca-35 <kartacak35@gmail.com>
2024-09-05 22:02:02 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,228 @@
+document.addEventListener('DOMContentLoaded', async () => {
+    const searchBox = document.getElementById('searchBox');
+    const resultDiv = document.getElementById('result');
+    const ghostText = document.getElementById('ghostText');
+    const searchContainer = document.querySelector('.search-box');
+    const wordCountElement = document.getElementById('wordCount');
+
+    let dictionaryData = {};
+    let lastQuery = '';
+    let hasError = false;
+
+    const clickableWords = {
+        "+k": ["<span class='red'>geçişsiz eylem</span> yan-", "<span class='red'>ad</span> yanık", "<span class='red'>bağlaç</span> kelimesi", "<span class='red'>fiil</span> anlamı"],
+        "mezcetmek": ["örnek anlam 1", "örnek anlam 2", "örnek anlam 3", "örnek anlam 4"],
+    };
+
+    try {
+        const response = await fetch('vocabulary.json');
+        if (!response.ok) {
+            throw new Error('Yoksa bir yerlerde bir harf mi kayıp?');
+        }
+        dictionaryData = await response.json();
+
+        const wordCount = Object.keys(dictionaryData).length;
+        wordCountElement.innerHTML = `Türk dilinin <span class="highlight">${wordCount}</span> maddelik arkeolojisi.`;
+    } catch (error) {
+        console.error('Yoksa bir yerlerde bir harf mi kayıp?', error);
+        hasError = true;
+
+        wordCountElement.innerHTML = `<p class="error-message">Yoksa bir yerlerde bir harf mi kayıp?</p>`;
+
+        searchContainer.classList.add('error');
+        resultDiv.classList.add('hidden');
+        ghostText.classList.add('hidden');
+    }
+
+    function searchWord(query) {
+        if (query === lastQuery) {
+            return;
+        }
+        lastQuery = query;
+
+        resultDiv.innerHTML = '';
+
+        if (query.startsWith(' ') || query.trim().length === 0) {
+            if (query.length === 0) {
+                searchContainer.classList.remove('error');
+                ghostText.textContent = "";
+                return;
+            }
+            searchContainer.classList.add('error');
+            ghostText.textContent = "";
+            return;
+        } else {
+            searchContainer.classList.remove('error');
+        }
+
+        const normalizedQuery = normalizeTurkish(query);
+
+        const sortedWords = Object.keys(dictionaryData)
+            .map(word => ({ word: normalizeTurkish(word), original: word }))
+            .sort((a, b) => a.word.localeCompare(b.word));
+
+        const closestWord = sortedWords
+            .find(({ word }) => word.startsWith(normalizedQuery));
+
+        if (closestWord) {
+            const wordDetails = dictionaryData[closestWord.original];
+            const description = wordDetails.a.replace(/\n/g, "<br>");
+            const descriptionElement = document.createElement('p');
+            descriptionElement.classList.add('description');
+            descriptionElement.innerHTML = highlightWords(sanitizeHTML(description));
+            resultDiv.appendChild(descriptionElement);
+
+            const descriptionHeight = descriptionElement.offsetHeight;
+            descriptionElement.style.maxHeight = `${descriptionHeight}px`;
+
+            // Fade-in animasyonu ekleniyor
+            resultDiv.style.animation = 'fadeIn 1s ease-in-out';
+
+            ghostText.textContent = closestWord.word.substring(query.length);
+        } else {
+            ghostText.textContent = "";
+            searchContainer.classList.add('error');
+        }
+
+        resultDiv.style.animation = 'none';
+        resultDiv.offsetHeight;
+        resultDiv.style.animation = 'fadeIn 1s ease-in-out';
+
+        createClickableWords();
+    }
+
+    function createClickableWords() {
+        Object.keys(clickableWords).forEach(word => {
+            const regex = new RegExp(`(${word.replace(/([.*+?^=!:${}()|\[\]\/\\])/g, "\\$1")})`, 'gi');
+            resultDiv.innerHTML = resultDiv.innerHTML.replace(regex, `<span class="clickable-word" style="color: #e9d677; cursor: pointer;">$1</span>`);
+        });
+
+        const clickableElements = document.querySelectorAll('.clickable-word');
+
+        clickableElements.forEach(element => {
+            element.addEventListener('click', function () {
+                const word = this.textContent;
+                this.style.textDecoration = 'underline'; // Altı çizili yap
+                showWordMeanings(word, this);
+            });
+        });
+    }
+
+    function showWordMeanings(word, element) {
+        const meanings = clickableWords[word];
+        if (meanings && meanings.length > 0) {
+            const randomMeanings = getRandomMeanings(meanings, 2); // Rastgele 2 anlam seçiliyor
+            const tooltip = document.createElement('div');
+            tooltip.className = 'tooltip';
+            tooltip.innerHTML = randomMeanings.map(meaning => `<p>${meaning}</p>`).join('');
+
+            document.body.appendChild(tooltip);
+
+            const elementRect = element.getBoundingClientRect();
+            tooltip.style.position = 'absolute';
+            tooltip.style.display = 'block';
+
+            const tooltipRect = tooltip.getBoundingClientRect();
+            const top = elementRect.top + window.scrollY - tooltipRect.height - 5;
+            const left = elementRect.left + window.scrollX + (elementRect.width / 2) - (tooltipRect.width / 2);
+
+            tooltip.style.top = `${top}px`;
+            tooltip.style.left = `${left}px`;
+
+            tooltip.style.opacity = 0;
+            tooltip.style.transition = 'opacity 0.3s ease-in-out';
+            setTimeout(() => {
+                tooltip.style.opacity = 1;
+            }, 50);
+
+            element.style.textDecoration = 'underline';
+
+            element.addEventListener('mouseleave', function () {
+                tooltip.style.opacity = 0;
+                setTimeout(() => {
+                    tooltip.remove();
+                    element.style.textDecoration = 'none';
+                }, 300);
+            });
+        }
+    }
+
+    function getRandomMeanings(meanings, count) {
+        const shuffled = meanings.sort(() => 0.5 - Math.random());
+        return shuffled.slice(0, count);
+    }
+
+    function normalizeTurkish(text) {
+        return text.replace(/İ/g, 'i').replace(/I/g, 'ı').toLowerCase();
+    }
+
+    function sanitizeHTML(htmlString) {
+        return DOMPurify.sanitize(htmlString, {
+            ALLOWED_TAGS: ['b', 'span', 'i', 'em', 'strong', 'a', 'br'],
+            ALLOWED_ATTR: ['href', 'class'],
+        });
+    }
+
+    function highlightWords(text) {
+        const specialWords = {
+            '01': 'Ana Yalnıkça',
+            '02': 'Ana Türkçe',
+            '03': 'Çocuk Ağzı',
+            '04': 'Yansımalı',
+            '05': 'Türkçe',
+            '06': 'Azerbaycan Türkçesi',
+        };
+
+        let markedText = text;
+        for (const [key, value] of Object.entries(specialWords)) {
+            const regex = new RegExp(`\\b${key}\\b`, 'gi');
+            markedText = markedText.replace(regex, (match) => `[SPECIAL:${key}]`);
+        }
+
+        let resultText = markedText;
+        for (const [key, value] of Object.entries(specialWords)) {
+            const regex = new RegExp(`\\[SPECIAL:${key}\\](\\s+)(\\S+)`, 'gi');
+            resultText = resultText.replace(regex, (match, p1, p2) => `<b>${value}</b>${p1}<span class="purple">${p2}</span>`);
+        }
+
+        resultText = resultText.replace(/\[SPECIAL:\S+\]/g, '');
+
+        return resultText;
+    }
+
+    function updateSearchBoxPlaceholder(query) {
+        const queryLower = normalizeTurkish(query);
+        const matchingWord = Object.keys(dictionaryData)
+            .map(word => ({ word: normalizeTurkish(word), original: word }))
+            .sort((a, b) => a.word.localeCompare(b.word))
+            .find(({ word }) => word.startsWith(queryLower));
+
+        if (matchingWord) {
+            const remainingPart = matchingWord.word.substring(query.length);
+            ghostText.textContent = remainingPart;
+
+            const inputRect = searchBox.getBoundingClientRect();
+            const inputStyle = window.getComputedStyle(searchBox);
+            const paddingLeft = parseFloat(inputStyle.paddingLeft);
+            const fontSize = parseFloat(inputStyle.fontSize);
+
+            const firstCharWidth = getTextWidth(query, fontSize);
+            ghostText.style.left = `${paddingLeft + firstCharWidth}px`;
+        } else {
+            ghostText.textContent = "";
+        }
+    }
+
+    function getTextWidth(text, fontSize) {
+        const canvas = document.createElement('canvas');
+        const context = canvas.getContext('2d');
+        context.font = `${fontSize}px 'Poppins', sans-serif`;
+        return context.measureText(text).width;
+    }
+
+    searchBox.addEventListener('input', () => {
+        const query = searchBox.value;
+        updateSearchBoxPlaceholder(query);
+        searchWord(query);
+    });
+});
